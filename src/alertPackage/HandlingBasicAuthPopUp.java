324cf491a8b563ad53c9f5bba3e0cbeb0dde28e9
Pritam Pramanick <ijiopritam@gmail.com>
2026-01-28 17:12:12 +0530

    Basic Auth1

--- src/alertPackage/HandlingBasicAuthPopUp.java
+++ src/alertPackage/HandlingBasicAuthPopUp.java
@@ -1,4 +1,23 @@
 package alertPackage;
 
-public class HandlingBasicAuthPopUp {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandlingBasicAuthPopUp
+{
+    public static void main(String[] args)
+    {
+      try
+      {
+          WebDriver driver = new ChromeDriver();
+
+          driver.manage().window().maximize();
+
+          driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+      }
+      catch (Exception e)
+      {
+          e.printStackTrace();
+      }
+    }
 }
